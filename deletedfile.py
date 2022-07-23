f545aca9b9c98cfbaa4915d38d7ff99865978cbb
Abd_ElrahmanAyman <99566868+AbdoEldgwy@users.noreply.github.com>
2022-07-23 06:44:55 +0200

    Add files via upload
    idea for making simple Database

---
+++ deletedfile.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+DataName = []
+DataAge = []
+DataSpec = []
+DataEmail = []
+
+while True:
+    inpName = input("type your Name: ")
+    inpAge = input("type your age: ")
+    inpSpecialty = input("type your specialty: ")
+    email = f"{inpName}{inpSpecialty}{inpAge}@ai.com"
+
+    DataName.append(inpName)
+    DataAge.append(inpAge)
+    DataSpec.append(inpSpecialty)
+    DataEmail.append(email)
+
+    RowData = {"name": DataName,
+               "age": DataAge,
+               "specialty": DataSpec,
+               "Email": email}
+
+    df1 = pd.DataFrame(RowData)
+    print(df1)
